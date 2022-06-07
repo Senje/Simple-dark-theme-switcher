100645a5ea205491f834544a8e9709dbd31107bc
Senje <103327356+Senje@users.noreply.github.com>
2022-06-07 10:33:40 +1000

    Add files via upload

---
+++ dark.js
@@ -0,0 +1,67 @@
+// Default div class for button
+const button = '.dark';
+// ID for <style>
+const styleID = 'color-switcher';
+// name for localStorage item
+const storageItem = 'dark';
+
+/* vars for setting dark theme color */
+const backgroundColor = '#212529';
+const fontColor = '#f4f3ef';
+
+const divs = {
+    'body': {
+        'background-color': backgroundColor,
+        'color': fontColor
+    }
+}
+
+// Return to black if it was be chose
+if (localStorage.getItem(storageItem)) {
+    switchToBlack();
+}
+
+
+// Listiner for button
+
+window.onload = function () {
+    let element = document.querySelector(button);
+    element.addEventListener('click', function () {
+        var item = localStorage.getItem(storageItem);
+        if (parseInt(item) === 1) {
+            switchToDefault();
+        } else {
+            switchToBlack();
+        }
+    });
+}
+
+// function for switching to black theme
+function switchToBlack() {
+
+    localStorage.setItem(storageItem, 1);
+
+    // Change background colors
+    let elements = '';
+    for (let [key, value] of Object.entries(divs)) {
+        elements += key + '{';
+        // Get properties
+        for (let [attribute, properties] of Object.entries(value)) {
+            elements += attribute + ':' + properties + ' !important;';
+        }
+        elements += '}';
+    }
+
+    let style = document.createElement('style');
+    style.setAttribute('id', styleID);
+    document.head.append(style);
+    style.innerHTML = elements;
+    
+}
+
+// Return to default theme
+function switchToDefault() {
+    let element = document.querySelector('#' + styleID);
+    localStorage.removeItem(storageItem);
+    element.remove();
+}
